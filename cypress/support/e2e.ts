9c5368388b2541f2b67c2c8f5042d7673d2040b4
olka bocheńska <olka.bochenska93@gmail.com>
2023-09-19 11:23:33 +0200

    use custom commands instead of page object

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,37 @@
-import './commands';
+import './commands/login';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to enter username
+       * @example cy.enterUsername(username)
+       */
+      enterUsername(username: string): void;
+
+      /**
+       * Custom command to enter password
+       * @example cy.enterPassword(password)
+       */
+      enterPassword(password: string): void;
+
+      /**
+       * Custom command to click login button
+       * @example cy.clickLoginButton()
+       */
+      clickLoginButton(): void;
+
+      /**
+       * Custom command to login.
+       * @example cy.login(username, password)
+       */
+      login(username: string, password: string): void;
+
+      /**
+       * Custom command to clear login form.
+       * @example cy.loginFormClear()
+       */
+      loginFormClear(): void;
+    }
+  }
+}
